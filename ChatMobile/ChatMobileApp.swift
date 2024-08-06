fc5ec9e658012b3362cd4873675a14de336f8290
Sergey Dyatlov <sergeyd020501@gmail.com>
2024-08-07 00:34:50 +0300

    Initial Commit

---
+++ ChatMobile/ChatMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMobileApp.swift
+//  ChatMobile
+//
+//  Created by Сергей Дятлов on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
